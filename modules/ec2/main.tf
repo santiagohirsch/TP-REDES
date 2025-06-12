f4cd1cd27878dcca02065864fa8a004945112e1d
Santiago Tomas Medin <smedin@itba.edu.ar>
2025-06-11 23:48:57 -0300

    feat: elastic ip

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,20 +1,37 @@
-resource "aws_instance" "this" {
-    ami                    = "ami-0731becbf832f281e"
-    instance_type          = var.instance_type
-    subnet_id              = var.subnet_id
-    key_name               = var.key_name
-    vpc_security_group_ids = var.security_group_ids
+#########################################
+###           EC2 INSTANCE            ###
+#########################################
 
+resource "aws_instance" "this" {
+    ami                         = "ami-0731becbf832f281e"
+    instance_type               = var.instance_type
+    subnet_id                   = var.subnet_id
+    key_name                    = var.key_name
+    vpc_security_group_ids      = var.security_group_ids
     associate_public_ip_address = var.public
+
     user_data = templatefile("${path.module}/scripts/mlflow-server.sh", {
-        db_user       = var.rds_username
-        db_password   = var.rds_password
-        db_endpoint   = var.rds_endpoint
-        db_name       = var.rds_db_name
+        db_user        = var.rds_username
+        db_password    = var.rds_password
+        db_endpoint    = var.rds_endpoint
+        db_name        = var.rds_db_name
         mlflow_service = file("${path.module}/scripts/service.txt")
     })
 
     tags = {
         Name = var.instance_name
+    }
+}
+
+#########################################
+###             EIP                   ###
+#########################################
+
+resource "aws_eip" "this" {
+    instance = aws_instance.this.id
+    domain   = "vpc"
+
+    tags = {
+        Name = "${var.instance_name}-eip"
     }
 }
--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -3,5 +3,5 @@ output "id" {
 }
 
 output "public_ip" {
-    value = aws_instance.this.public_ip
+    value = aws_eip.this.public_ip
 }
